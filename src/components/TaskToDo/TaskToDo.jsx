13e7849aa295a12ddee8162c6653922017ff22c6
Erik Silcox <eriksilcox651@gmail.com>
2024-03-28 00:37:59 -0500

    Put and Delete created, working.

--- src/components/TaskToDo/TaskToDo.jsx
+++ src/components/TaskToDo/TaskToDo.jsx
@@ -39,6 +39,34 @@ function TaskToDo() {
       });
   };
 
+  //delete Task
+  const removeTask = (id) => {
+    console.log(id, 'removeTask');
+    axios
+      .delete(`/api/todo/${id}`)
+      .then((response) => {
+        fetchTask();
+      })
+      .catch((error) => {
+        console.log(error, `error`);
+        alert(`Something went wrong`);
+      });
+  };
+
+  // mark task as completed
+  const toggleTask = (id) => {
+    console.log('toggle', id);
+    axios
+      .put(`api/todo/${id}`)
+      .then((response) => {
+        fetchTask();
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Something went wrong');
+      });
+  };
+
   return (
     <section className="new-task-section">
       <form onSubmit={addTask}>
@@ -51,12 +79,40 @@ function TaskToDo() {
 
         <button type="submit">Submit New Task</button>
       </form>
-      <p>Click Submit New Task to add, "{taskToDoTaskName}" to Task To DO List</p>
-      <ul>
-        {taskToDoArray.map((task) => {
-          return <li key={task.id}>{task.taskName}</li>;
-        })}
-      </ul>
+      <p>
+        Click Submit New Task to add, "{taskToDoTaskName}" to Task To DO List
+      </p>
+      {taskToDoArray.map((task) => (
+        <div
+          className={task.isCompleted ? 'completed' : 'not-completed'}
+          key={task.id}
+        >
+          {task.taskName} + is + {task.isCompleted}
+          <button
+            style={{ backgroundColor: 'green' }}
+            onClick={() => removeTask(task.id)}
+          >
+            Delete
+          </button>
+          {task.isCompleted ? (
+            <button
+              style={{ backgroundColor: 'blue' }}
+              onClick={() => toggleTask(task.id)}
+            >
+              Undo
+            </button> //else
+          ) : (
+            <button
+              style={{ backgroundColor: 'red' }}
+              onClick={() => toggleTask(task.id)}
+            >
+              To Do
+            </button>
+          )}
+          {task.isCompleted && <div>Tasks I've Completed</div>}
+          {/*return <li key={task.id}>{task.taskName}</li>;*/}
+        </div>
+      ))}
     </section>
   );
 }
